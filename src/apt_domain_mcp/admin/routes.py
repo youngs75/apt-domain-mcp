5ad6c1dee010b481f8912320edfd6a3f286d95bb
youngs75 <youngs75.kim@samsung.com>
2026-04-17 14:12:41 +0900

    Revert "feat: admin REST API + dashboard UI + ingest service extraction"
    This reverts commit 617eab147d5a963d7c8fd5927e388b15a66d5aed.

--- src/apt_domain_mcp/admin/routes.py
+++ src/apt_domain_mcp/admin/routes.py
@@ -7,7 +7,6 @@ from starlette.requests import Request
 from starlette.responses import FileResponse, Response
 from starlette.routing import Route
 
-from .api import api_routes
 from .auth import handle_login, handle_logout
 
 _STATIC_DIR = Path(__file__).parent / "static"
@@ -26,5 +25,5 @@ admin_routes: list[Route] = [
     Route("/login", handle_login, methods=["GET", "POST"]),
     Route("/logout", handle_logout, methods=["POST"]),
     Route("/", serve_admin_page),
-    *api_routes,
+    # API routes will be added in step 2
 ]
